4ac74cb20c1491f75b6c9d0291d2ba5f4c846f4c
shruti-linux <131751004+shruti-linux@users.noreply.github.com>
2026-05-13 13:18:10 +0530

    Update LoginBasicTest.java

--- src/test/java/tests/LoginBasicTest.java
+++ src/test/java/tests/LoginBasicTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
